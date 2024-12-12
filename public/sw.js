570bb5f4ecd328883522d70ba4927a72904cdadc
leqos7 <giorgi.giorgadze@gmail.com>
2024-12-12 20:56:25 +0400

    Fixed PWA configuration and paths

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,11 @@
 const CACHE_NAME = 'rmp-v1';
+const BASE_PATH = '/leqso';
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  BASE_PATH + '/',
+  BASE_PATH + '/index.html',
+  BASE_PATH + '/manifest.json',
+  BASE_PATH + '/favicon.ico'
 ];
 
 self.addEventListener('install', (event) => {
@@ -15,6 +18,23 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
   );
 });
