6f7c324c19956af4529271977cb0c75bf817caed
leqos7 <giorgi.giorgadze@gmail.com>
2024-12-12 21:23:14 +0400

    Fixed import paths and added TypeScript configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -15,18 +15,18 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <BrowserRouter basename="/leqso">
+        <Toaster />
+        <Sonner />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/content" element={<Content />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
-      <CountdownTimer />
-      <Clock />
-      <InstallPWA />
-    </BrowserRouter>
+        <CountdownTimer />
+        <Clock />
+        <InstallPWA />
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
